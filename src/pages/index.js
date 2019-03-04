ca9bcb4131881eef64d5d69201ba86615cc9afc2
Jacob Impson <jacobimpson@gmail.com>
2019-03-04 17:22:10 +0800

    failed netlify build, inline data:svg rather than querying for it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 import PrivacyPolicy from "../pages/privacy-policy"
 
 const IndexPage = () => <PrivacyPolicy />
